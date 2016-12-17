e6c18c28d0ca771581641eb9aa5f9f9371218afb
Chris Gradwohl <christophergradwohl@gmail.com>
2016-12-17 01:04:08 -0800

    added index.html, scss, css

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
-'use strict'
+"use strict"
 
 var express = require('express');
-
 var app = express();
 
+// api router
 
 
-app.listen(3000, function(req, res) {
-	res.send('hello creatures....');
+// api routes
+app.use('/', express.static('public'));
+
+app.get('/api/yodawg', function(req, res) {
+	res.send('hello creature....');
+	res.json({todos:[]});
+});
+
+
+
+app.listen(3000, function() {
+	console.log('hello creatures....');
 });
